b20ec907a4e748aeecf834edad95986918e70d0e
Athena Zimmerman <bazman.bz@gmail.com>
2023-01-27 13:57:14 -0500

    Server receives and responds with multiple channels

--- server.ts
+++ server.ts
@@ -9,10 +9,9 @@ const KEY = process.env.API_KEY;
 const app = express();
 const port = 8080;
 
-// implement a detailed error response
 app.get('/', async (req, res) => {
-  const channelName = req.query.name as string;
-  const ID = await new Promise((resolve) => {
+  const channelNames: string[] = (req.query.name as string).split('%25');
+  const getID = (channelName: string) => new Promise((resolve) => {
     https.get(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&eventType=live&q=${channelName?.split(' ').join('%20')}&type=video&key=${KEY}`, (resp) => {
       let data = '';
 
@@ -21,25 +20,26 @@ app.get('/', async (req, res) => {
       });
 
       resp.on('end', () => {
-        const liveID = JSON.parse(data)?.items[0]?.id?.videoId ?? 'No live stream found';
-
-        const response = {
-          status: resp.statusCode,
-          headers: resp.headers,
-          body: liveID,
-        };
-
-        resolve(JSON.stringify(response));
+        resolve(JSON.parse(data)?.items[0]?.id?.videoId ?? 'No live stream found');
       });
     });
   });
+
+  const idArray = channelNames.map(async (channelName) => getID(channelName));
+
+  const IDs = await Promise.all(idArray);
+
+  const response = {
+    status: 'success',
+    channels: IDs,
+  };
 
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
 
-  res.send(ID);
+  res.send(response);
 });
 
 app.listen(port, () => {
